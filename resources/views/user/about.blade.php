5baa77e2653795cda707e5c57e680c654f98061e
afghanbahri <afghanbahri@gmail.com>
2022-12-21 21:11:22 +0700

    .

--- resources/views/user/about.blade.php
+++ resources/views/user/about.blade.php
@@ -22,53 +22,49 @@
         <div class="row">
             <div class="col-lg-7">
                 <div class="featured-text">
-                    <h2 class="pb-3">Why <span class="orange-text">Brainy</span>?</h2>
+                    <h2 class="pb-3">Why <span class="orange-text">BUYMAXCO</span>?</h2>
                     <div class="row">
                         <div class="col-lg-6 col-md-6 mb-4 mb-md-5">
                             <div class="list-box d-flex">
                                 <div class="list-icon">
-                                    <i class="fas fa-shipping-fast"></i>
+                                    <i class="fas fa-clipboard"></i>
                                 </div>
                                 <div class="content">
-                                    <h3>Fast Service</h3>
-                                    <p>We don't like to keep customers waiting, so we always serve customers as quickly
-                                        as possible.</p>
+                                    <h3>Kategori</h3>
+                                    <p>Sepatu akan dibedakan berdasarkan kategorinya seperti Ventela, Compass, Warrior dan Patrobas.</p>
                                 </div>
                             </div>
                         </div>
                         <div class="col-lg-6 col-md-6 mb-5 mb-md-5">
                             <div class="list-box d-flex">
                                 <div class="list-icon">
-                                    <i class="fas fa-money-bill-alt"></i>
+                                    <i class="fas fa-clock"></i>
                                 </div>
                                 <div class="content">
-                                    <h3>Best Price</h3>
-                                    <p>We always provide the best prices in our menu, of course with the best quality
-                                        and reasonable prices.</p>
+                                    <h3>Up to-date</h3>
+                                    <p>Sepatu populer dan terbaru akan selalu up to date setiap harinya.</p>
                                 </div>
                             </div>
                         </div>
                         <div class="col-lg-6 col-md-6 mb-5 mb-md-5">
                             <div class="list-box d-flex">
                                 <div class="list-icon">
-                                    <i class="fas fa-glass-cheers"></i>
+                                    <i class="fas fa-check-square"></i>
                                 </div>
                                 <div class="content">
-                                    <h3>No Sizing</h3>
-                                    <p>You hate the sizes in the menu, so do we. we always use the largest size in our
-                                        menu.</p>
+                                    <h3>Wishlist</h3>
+                                    <p>Tandai sepatu yang anda suka tanpa batas.</p>
                                 </div>
                             </div>
                         </div>
                         <div class="col-lg-6 col-md-6">
                             <div class="list-box d-flex">
                                 <div class="list-icon">
-                                    <i class="fas fa-cloud-sun"></i>
+                                    <i class="fas fa-cart-plus"></i>
                                 </div>
                                 <div class="content">
-                                    <h3>Beautiful View </h3>
-                                    <p>Enjoy the beautiful view while eating the menu while chatting with the people
-                                        closest to you.</p>
+                                    <h3>Keranjang</h3>
+                                    <p>Memberikan informasi total dari harga sepatu yang telah ditandai oleh pengguna.</p>
                                 </div>
                             </div>
                         </div>
@@ -93,11 +89,11 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-lg-4 col-md-6">
+            <div class="col-lg-3 col-md-6">
                 <div class="single-team-item">
                     <div class="team-bg team-bg-1"></div>
-                    <h4>Agni Pangestu 
-						<span>Barista</span>
+                    <h4>Afghan M Bahri 
+						<span>152019013</span>
 					</h4>
                     <ul class="social-link-team">
                         <li><a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
@@ -106,11 +102,11 @@
                     </ul>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-6">
+            <div class="col-lg-3 col-md-6">
                 <div class="single-team-item">
                     <div class="team-bg team-bg-2"></div>
-                    <h4>Galih Respati Permana 
-						<span>Cashier</span>
+                    <h4>Nicolaus Buha Kristian S
+						<span>152019022</span>
 					</h4>
                     <ul class="social-link-team">
                         <li><a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
@@ -119,11 +115,24 @@
                     </ul>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-6 offset-md-3 offset-lg-0">
+            <div class="col-lg-3 col-md-6 offset-md-3 offset-lg-0">
                 <div class="single-team-item">
                     <div class="team-bg team-bg-3"></div>
-                    <h4>Ridwan Alfalhan 
-						<span>Owner</span>
+                    <h4>Rio Dwianto
+						<span>152019026</span>
+					</h4>
+                    <ul class="social-link-team">
+                        <li><a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+                        <li><a href="#" target="_blank"><i class="fab fa-twitter"></i></a></li>
+                        <li><a href="#" target="_blank"><i class="fab fa-instagram"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 offset-md-3 offset-lg-0">
+                <div class="single-team-item">
+                    <div class="team-bg team-bg-4"></div>
+                    <h4>M Ariel Fadhilah F
+						<span>152019030</span>
 					</h4>
                     <ul class="social-link-team">
                         <li><a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
